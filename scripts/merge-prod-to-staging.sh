41ea9c93e42e20ac4149967fc9f0d442d0ab2057
domitilleprevost <domitille.prevost@gmail.com>
2019-09-25 19:54:16 +0200

    tests_push

--- scripts/merge-prod-to-staging.sh
+++ scripts/merge-prod-to-staging.sh
@@ -34,5 +34,6 @@ git fetch
 git checkout staging
 git merge master
 git push
+git push https://${GITHUB_ACCESS_TOKEN}@github.com/hinosxz/chowchow.git
 git remote add origin-staging https://${GITHUB_ACCESS_TOKEN}@github.com/hinosxz/chowchow.git > /dev/null 2>&1
-git push --set-upstream origin-staging
+git push --set-upstream origin-staging "${build_head}"
