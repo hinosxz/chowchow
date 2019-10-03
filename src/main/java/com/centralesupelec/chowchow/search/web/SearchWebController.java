28e4a146eb2471a5dc374becfd27fb2e59c10626
hinosxz <gauthier.henon@datadoghq.com>
2019-10-03 20:08:28 +0200

    Handle errors

--- src/main/java/com/centralesupelec/chowchow/search/web/SearchWebController.java
+++ src/main/java/com/centralesupelec/chowchow/search/web/SearchWebController.java
@@ -1,9 +1,9 @@
 package com.centralesupelec.chowchow.search.web;
 
 import com.centralesupelec.chowchow.search.controllers.SearchController;
-import com.centralesupelec.chowchow.search.controllers.TraktSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,7 +18,7 @@ public class SearchWebController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public TraktSearch[] findShowsByName(@RequestParam(value = "name") String name) {
+    public ResponseEntity findShowsByName(@RequestParam(value = "name") String name) {
         return this.searchController.findShowsByName(name);
     }
 }
