c27193fbab8e391f54b824c2ba3a875882ce3d13
hinosxz <gauthier.henon@datadoghq.com>
2019-10-22 14:32:01 +0200

    Add antdesign UI lib

--- src/main/javascript/src/components/home/PageHome.tsx
+++ src/main/javascript/src/components/home/PageHome.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
+import { Button } from 'antd';
 
 import './page-home.scss';
 
 export const PageHome: React.FC = () => (
   <div className="Home">
-    <header className="Home-header">
-      <a
-        className="Home-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-          Learn React
-      </a>
-    </header>
+    <Button>Welcome!</Button>
   </div>
 );
