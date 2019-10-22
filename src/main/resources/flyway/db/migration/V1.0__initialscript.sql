d7a1560d4680692f2b1127400b8aa5a49227c67c
prevostdomitille <domitille.prevost@gmail.com>
2019-10-22 15:59:01 +0200

    first migration

--- src/main/resources/flyway/db/migration/V1.0__initialscript.sql
+++ src/main/resources/flyway/db/migration/V1.0__initialscript.sql
@@ -1,30 +1,16 @@
-DROP TABLE IF EXISTS Shows CASCADE;
-CREATE TABLE Shows (
-  id SERIAL,
-  tmdb_id INTEGER NOT [NULL UNIQUE,
-  name TEXT NOT NULL,
-  year INTEGER NOT NULL,
-  PRIMARY KEY (id)
-);
-
 DROP TABLE IF EXISTS Users CASCADE;
 CREATE TABLE Users (
   id SERIAL,
   username TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL ,
-  type TEXT NOT NULL,
-  subscription_type TEXT,
+  subscription_type INTEGER ,
   PRIMARY KEY (id)
 );
 
-INSERT INTO public.Shows (id, tmdb_id, name, year)
-VALUES
-(1, 60574, 'Peaky Blinders', 2018),
-(2, 62688, 'Supergirl', 2019),
-(3, 71789, 'Seal TEAM', 2008);
-
-INSERT INTO public.Users (id, username, password, type, subscription_type)
-VALUES
-(1, 'User_1', 'password_1', 'USER', NULL),
-(2, 'User_2', 'password_2', 'PREMIUM_USER', 'BASIC'),
-(3, 'User_3', 'password_3', 'PREMIUM_USER', 'GOLD');
+DROP TABLE IF EXISTS likes CASCADE;
+CREATE TABLE likes (
+  user_id SERIAL NOT NULL,
+  show_id SERIAL NOT NULL,
+  mark INTEGER,
+  FOREIGN KEY (user_id) REFERENCES Users(id) ON UPDATE CASCADE
+);
