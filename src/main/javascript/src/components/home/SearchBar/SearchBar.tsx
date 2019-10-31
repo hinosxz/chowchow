befd796329fd2d8383d46873bb47a0f163ecbf83
hinosxz <gauthier.henon@datadoghq.com>
2019-10-31 15:43:41 +0100

    Add select component

--- src/main/javascript/src/components/home/SearchBar/SearchBar.tsx
+++ src/main/javascript/src/components/home/SearchBar/SearchBar.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Async from 'react-select/async';
 
 import { getSearch } from 'lib/api/search';
 
-export const SearchBar: React.FunctionComponent = () => {
-  React.useEffect(() => {
-    getSearch('12 monkeys').then(res => {
-      console.log(res);
-    });
-  }, []);
-  return null;
-};
+function loadOptions(name: string) {
+  return getSearch(name).then(res => res.results.map(show => ({
+    label: show.name,
+    value: show,
+  })));
+}
+
+export const SearchBar: React.FunctionComponent = () => (
+  <Async
+    cacheOptions
+    defaultOptions={[]}
+    loadOptions={loadOptions}
+    noOptionsMessage={() => null}
+    placeholder="Search shows..."
+  />
+);
