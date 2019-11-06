4d026ab31bd0017456d924eb58f1291b8c72b458
hinosxz <gauthier.henon@datadoghq.com>
2019-11-06 15:33:42 +0100

    Add link to show

--- src/main/javascript/src/components/show-list/ShowItem/ShowItem.tsx
+++ src/main/javascript/src/components/show-list/ShowItem/ShowItem.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Col, Divider, Row } from 'antd';
+import {
+  Button, Col, Divider, Icon, Row, Typography,
+} from 'antd';
+import { Link } from 'react-router-dom';
 
 import { Like } from 'lib/types';
+import { Mark } from 'components/ui/Mark/Mark';
 
 import './show-item.scss';
 
 const BLOCK = 'show-list_show-item';
+const { Paragraph, Title } = Typography;
 
 interface ShowItemProps {
   like: Like;
@@ -18,7 +23,17 @@ export const ShowItem: React.FunctionComponent<ShowItemProps> = ({ like }) => (
         <img className={`${BLOCK}__poster__image`} src={like.show.posterPath} alt="Poster" />
       </Col>
       <Col className={`${BLOCK}__description`} span={20}>
-        {like.show.name}
+        <Title level={3}>{like.show.name}</Title>
+        <Mark mark={like.mark} />
+        <Paragraph>{like.show.overview}</Paragraph>
+        <div className={`${BLOCK}__go-to-button`}>
+          <Link to={`shows/${like.show.id}`}>
+            <Button type="primary">
+              Go to show
+              <Icon type="right" />
+            </Button>
+          </Link>
+        </div>
       </Col>
     </Row>
     <Divider />
