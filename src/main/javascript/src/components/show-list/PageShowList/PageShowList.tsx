50893b95ba31672ad1d142dd4942d3a9ed3b7274
hinosxz <gauthier.henon@datadoghq.com>
2019-11-06 15:39:08 +0100

    Add page title

--- src/main/javascript/src/components/show-list/PageShowList/PageShowList.tsx
+++ src/main/javascript/src/components/show-list/PageShowList/PageShowList.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Divider, Typography } from 'antd';
 
 import { Like } from 'lib/types';
 import { ShowItem } from 'components/show-list/ShowItem/ShowItem';
 import { api } from 'lib/api/api';
 
 const BLOCK = 'show-list_page-show-list';
+const { Title } = Typography;
 
 export const PageShowList: React.FunctionComponent = () => {
   const [data, setData] = React.useState<Like[]>([]);
@@ -15,6 +17,8 @@ export const PageShowList: React.FunctionComponent = () => {
 
   return (
     <div className={BLOCK}>
+      <Title level={2}>My favorite shows</Title>
+      <Divider />
       {data.map(like => <ShowItem key={like.show.id} like={like} />)}
     </div>
   );
