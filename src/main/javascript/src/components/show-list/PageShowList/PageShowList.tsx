c975c1b71d1f5e3719f026cce069b032065620b6
hinosxz <gauthier.henon@datadoghq.com>
2019-11-06 15:54:34 +0100

    Add placeholder

--- src/main/javascript/src/components/show-list/PageShowList/PageShowList.tsx
+++ src/main/javascript/src/components/show-list/PageShowList/PageShowList.tsx
@@ -4,22 +4,28 @@ import { Divider, Typography } from 'antd';
 import { Like } from 'lib/types';
 import { ShowItem } from 'components/show-list/ShowItem/ShowItem';
 import { api } from 'lib/api/api';
+import { Placeholder } from 'components/ui/Placeholder/Placeholder';
 
 const BLOCK = 'show-list_page-show-list';
 const { Title } = Typography;
 
 export const PageShowList: React.FunctionComponent = () => {
   const [data, setData] = React.useState<Like[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
-    api.get('users/likes').json<Like[]>().then(like => setData(like));
+    setIsLoading(true);
+    api.get('users/likes').json<Like[]>().then(like => {
+      setData(like);
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <div className={BLOCK}>
       <Title level={2}>My favorite shows</Title>
       <Divider />
-      {data.map(like => <ShowItem key={like.show.id} like={like} />)}
+      {isLoading ? <Placeholder /> : data.map(like => <ShowItem key={like.show.id} like={like} />)}
     </div>
   );
 };
