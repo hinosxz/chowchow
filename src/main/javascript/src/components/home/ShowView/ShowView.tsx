1343af25948ee487dfcbe2a29f4e88515294c8ad
donacrio <donatien.criaud@gmail.com>
2019-11-23 12:44:30 +0100

    Change typo

--- src/main/javascript/src/components/home/ShowView/ShowView.tsx
+++ src/main/javascript/src/components/home/ShowView/ShowView.tsx
@@ -52,14 +52,14 @@ export const ShowView: React.FunctionComponent<ShowViewProps> = ({ isLiked, show
             type="primary"
             onClick={() => postLikes(show.id).then(() => setIsLikeDisabled(true)).catch(setError)}
           >
-            Like
-            <Icon type="like" />
+            <Icon type="eye" />
+            Add to watch list
           </Button>
         </div>
 
         {error && (
           <Alert
-            message="Could not like this show. Please retry in a few seconds."
+            message="Could not add this show to your watch list. Please retry in a few seconds."
             type="error"
           />
         )}
